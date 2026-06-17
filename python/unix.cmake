487c8be0ed5972bbdf1a4901ef080ea88a7fb2dd
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2026-06-17 17:03:11 +0200

    (mac) development module

--- python/unix.cmake
+++ python/unix.cmake
@@ -60,6 +60,7 @@ set(configure_args
 if(APPLE)
     list(APPEND configure_args "--enable-framework=${PROJECT_BINARY_DIR}/${PYNCPP_PYTHON_SUBDIR}")
     list(APPEND configure_args "--with-framework-name=Python")
+    list(APPEND configure_args "--with-pydebug") # to get Development.Module (for VTK for instance)
 else()
     list(APPEND configure_args --enable-shared)
 endif()
